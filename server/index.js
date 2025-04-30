3555094aaaf672e38176e761e3a01629d08f3237
gkkary3 <nambawon1@naver.com>
2025-04-30 15:26:15 +0900

    Initial commit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ const roomRoutes = require("./routes/roomRoutes");
 const app = express();
 
 // 미들웨어 설정
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // MongoDB 연결
